bbe51911d56aee602c9f0efc0626abb76f84176b
Vishvesh Nagar <vishvesh@nagar.me>
2026-07-12 19:52:06 +0530

    Added input validation to ensure the user enters a valid number between 1 to 5.

--- Gussthenumber.py
+++ Gussthenumber.py
@@ -1,13 +1,22 @@
 import random
-while True:
 
+def get_player_choice():
     choices = ['1', '2', '3', '4', '5']
-    y=random.choice(choices)
-    x=input("Welcome to the number guessing game by Vishvesh Nagar!! Choose a number 1,2,3,4,5 or quit : ").lower()
-    if x == y:
-        print("you win go to vishvesh.nagar.me for more free games.")
-    elif x == 'quit':
+    while True:
+        x = input("Welcome to the number guessing game by Vishvesh Nagar!! Choose a number 1,2,3,4,5 or quit : ").strip().lower()
+        if x in choices or x == 'quit':
+            return x
+        print("Oops! Choose Correctly.")
+
+
+while True:
+    y = random.choice(['1', '2', '3', '4', '5'])
+    x = get_player_choice()
+
+    if x == 'quit':
         print("Thanks for playing!!go to vishvesh.nagar.me for more free games.")
         break
+    if x == y:
+        print("you win go to vishvesh.nagar.me for more free games.")
     else:
         print("you lose go to vishvesh.nagar.me for more free games.")
